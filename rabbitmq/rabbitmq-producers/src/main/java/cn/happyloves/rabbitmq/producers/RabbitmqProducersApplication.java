5187537ede04084f134ff0f211166ce5c728b19f
sky <452425952@qq.com>
2020-12-09 20:32:39 +0800

    commit code

---
+++ rabbitmq/rabbitmq-producers/src/main/java/cn/happyloves/rabbitmq/producers/RabbitmqProducersApplication.java
@@ -0,0 +1,13 @@
+package cn.happyloves.rabbitmq.producers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducersApplication.class, args);
+    }
+
+}
---
+++ rabbitmq/rabbitmq-producers/src/test/java/cn/happyloves/rabbitmq/producers/RabbitmqProducersApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.happyloves.rabbitmq.producers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProducersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
